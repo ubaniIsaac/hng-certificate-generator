6ebaae4e4eb502d5bc79670bbe648580f5bf106a
Fadlullah <fadlulanre@gmail.com>
2022-11-11 22:15:08 +0100

    added the starter files for the task

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(express.json());
+
+const port = 4000
+const start = async () => {
+    app.listen(port, () => {
+        console.log(`App running on port ${port} ......`)
+    })
+};
+
+start();
