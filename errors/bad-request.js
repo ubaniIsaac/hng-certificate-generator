d654dbd8a63c2259bfb50bc9a58a6fdca7a5397b
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2022-11-12 16:30:27 +0100

    Delete errors directory

--- errors/bad-request.js
+++
@@ -1,9 +0,0 @@
-import CustomError from './customError.js';
-
-class BadRequestError extends CustomError {
-  constructor(message) {
-    super(message);
-    this.statusCode = 400;
-  }
-}
-export default BadRequestError;
