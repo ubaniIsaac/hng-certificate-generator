76c3e1eaca11408af46f859e91917e7794d7c576
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2022-11-12 20:27:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 require('./routes/converter')(app);
 
-const port = 4000;
+const port = process.env.port || 4000;
 const start = async () => {
   app.listen(port, () => {
     console.log(`App running on port ${port} ......`);
