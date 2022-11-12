c9fe9ead09b1cb50e20db0afd846ebbb4ea3cb1f
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2022-11-12 16:30:41 +0100

    Delete uploadCsv.js

--- routes/uploadCsv.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { uploadCsv } from '../controllers/uploadCsv.js';
-import multer from 'multer';
-import os from 'os';
-const router = express.Router();
-const upload = multer({ dest: os.tmpdir() });
-router.post('/', upload.single('csv'), uploadCsv);
-export default router;
