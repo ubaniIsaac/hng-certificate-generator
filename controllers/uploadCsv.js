4448771285ae2d2220476081959ce131c97d8110
Abdulkabir sultan <abdulkabirsultan@gmail.com>
2022-11-12 15:58:36 +0100

    add csv to json functionality

--- controllers/uploadCsv.js
+++ controllers/uploadCsv.js
@@ -1,10 +1,29 @@
 import NotFoundError from '../errors/not-found.js';
-import path from 'path';
+import BadRequestError from '../errors/bad-request.js';
+import csvToJson from 'convert-csv-to-json';
 export const uploadCsv = async (req, res) => {
-  const csv = req.files.upload.name;
-
-  if (path.extname(csv) !== '.csv') {
-    throw new NotFoundError('No csv file found!, please input a csv file');
+  if (!req.file || Object.keys(req.file).length === 0) {
+    throw new NotFoundError('No files were uploaded');
   }
-  res.json({ message: 'success' });
+  const csv = req.file;
+  if (!csv?.originalname.includes('.csv')) {
+    throw new BadRequestError('No csv file found, please input csv file');
+  }
+
+  const json = csvToJson.fieldDelimiter(',').getJsonFromCsv(csv.path);
+
+  json.forEach((row) => {
+    if (!row.name) {
+      throw new BadRequestError('please input a name in your csv!!');
+    }
+    if (!row.date) {
+      throw new BadRequestError('please input a date in your csv!!');
+    }
+    if (!row.certificate_number) {
+      throw new BadRequestError(
+        'please input a certificate number in your csv!!'
+      );
+    }
+  });
+  res.json({ result: json, count: json.length });
 };
