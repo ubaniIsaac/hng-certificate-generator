57177a9a1b0bd50c0ad907fa3054350a9b2f5db0
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2022-11-12 16:35:54 +0100

    revert

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 //import packages
-import express from 'express';
-import cors from 'cors';
-import 'express-async-errors';
-
-//import Routes
-import uploadRoute from './routes/uploadCsv.js';
-import convertRoute from './routes/converter.js';
-import errorHandlerMiddleware from './Middlewares/error-handler.js';
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
-//External Middlewares
-app.use(
-  cors({
-    origin: '*',
-  })
-);
-//express Middlewares
+app.use(cors({
+    origin: '*'
+}));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-//Routes
-const baseUrl = '/api/v1';
-app.get('/', (req, res) => res.send('HNG CERTIFICATE GENERATOR'));
-app.use(`${baseUrl}/upload`, uploadRoute);
-app.use(`${baseUrl}/convert`, convertRoute);
-
-app.use(errorHandlerMiddleware);
-
-const port = process.env.PORT || 4000;
+const port = 4000;
 const start = async () => {
   app.listen(port, () => {
     console.log(`App running on port ${port} ......`);
